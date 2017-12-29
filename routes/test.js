cb9273b50a89e4c348e082480ca942b1ae97ea8b
oswaldo <oswaldo@css-giovannya.visionsoftware.loc>
2017-12-29 10:28:31 -0500

    enable cors | add new conection to sql server from staging env

--- routes/test.js
+++ routes/test.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const cors = require('cors')
 const {selectAll, execSp} = require('./../dbs/sql')
 
-router.get('/', (req, res) => {
+var whitelist = process.env.whitelist.split('|')
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
-  selectAll('stores', (err, data) => {
+router.get('/',cors(corsOptions) , (req, res, next) => {
+
+  selectAll('results.WorkShift', (err, data) => {
     if (err) {
       res.status(500).send(err)
     }
     res.send(data)
   })
-
 })
 
 router.get('/fromSp', (req, res) => {
--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const port = process.env.PORT || 3000
 const app = express()
 app.use(bodyParser.json())
 app.set('view engine', 'pug')
 
+app.use(function (req, res, next) {
+  req.headers.origin = req.headers.origin || req.headers.host
+  next()
+})
+
 require('./config/config')
+
+const port = process.env.PORT || 3000
 
 const testRoute = require('./routes/test')
 const securityRoute = require('./routes/security')
 const ad = require('./routes/ad')
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('index', {title: 'Api Malla Horaria', message: 'Binvenido al Api de malla horaria de las tiendas mango !'})
 })
 
 app.use('/test', testRoute)
